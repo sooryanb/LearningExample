8f9a4a36935e15be17cf67f08468edaac339015c
sooryanarayanan <sooryanarayanan@qburst.com>
2021-04-14 10:22:10 +0530

    Removed unwanted logs.

--- app/src/main/java/com/example/activitylifecycle/AirplaneModeChangedReceiver.kt
+++ app/src/main/java/com/example/activitylifecycle/AirplaneModeChangedReceiver.kt
@@ -8,10 +8,9 @@ import android.widget.Toast
 
 class AirplaneModeChangedReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        Log.d("test", "aero0 called")
+
         val isAirplaneModeEnabled = intent?.getBooleanExtra("state", false) ?: return
         if(isAirplaneModeEnabled){
-            Log.d("test", "aero called")
             Toast.makeText(context, "Airplane Mode activated", Toast.LENGTH_LONG).show()
         }else{
             Toast.makeText(context, "Airplane Mode Deactivated", Toast.LENGTH_LONG).show()
